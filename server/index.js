77d1ba46f1555403e4c3fa977538154f9e8025ad
yasanmadhuranga <yasanmkahangama@gmail.com>
2025-04-02 00:54:12 +0530

    mongoDB is connected

--- server/index.js
+++ server/index.js
@@ -1,14 +1,28 @@
 import express from "express";
 import dotenv from 'dotenv';
+import mongoose from 'mongoose';
+
+import userRouter from './routes/user.route.js';
+import postRouter from './routes/post.route.js';
+import commentRouter from './routes/comment.route.js';
 
 dotenv.config();
 
 const app = express();
 
-console.log(process.env.START);
+const port = process.env.PORT || 8000;
+const mongoUrl = process.env.MONGOURL;
 
-app.listen(3000, ()=>{
-    console.log("server is running");
+mongoose.connect(mongoUrl)
+  .then(() => console.log('MongoDB connected successfully!'))
+  .catch(err => console.error('MongoDB connection error:', err));
+
+app.use("/users", userRouter);
+app.use("/posts", postRouter);
+app.use("/comments", commentRouter);
+
+app.listen(port, ()=>{
+    console.log(`server is running on ${port}`);  
 })
 
 
