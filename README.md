136c0dec29388aca7da22a42a4f663e2b21e368c
yasanmadhuranga <yasanmkahangama@gmail.com>
2025-05-09 23:09:28 +0530

    upload blog post is done

--- README.md
+++ README.md
@@ -1,2 +1,15 @@
 # Mern_Blog_APP
 This is Blog App that is developing by React.js with Tailwind CSS, Node.js and MongoDB
+
+
+1)After Express 5 we can handle errors within index.js file.
+
+app.use((error, req, res, next) => {
+  res.status(error.status || 500);
+
+  res.json({
+    message: error.message || "Something went wrong!",
+    status: error.status,
+    stack: error.stack, // if we are not in production
+  });
+});
--- client/src/routes/WritePage.jsx
+++ client/src/routes/WritePage.jsx
@@ -1,18 +1,33 @@
-import React from 'react';
-import { useUser, useAuth } from "@clerk/clerk-react";
+import React from "react";
+import { useAuth, useUser } from "@clerk/clerk-react";
 import "react-quill-new/dist/quill.snow.css";
 import ReactQuill from "react-quill-new";
 import { useMutation } from "@tanstack/react-query";
 import axios from "axios";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { useNavigate } from "react-router-dom";
 import { toast } from "react-toastify";
+import Upload from "../components/Upload";
 
 const WritePage = () => {
-
   const { isLoaded, isSignedIn } = useUser();
   const [value, setValue] = useState("");
-  
+  const [cover, setCover] = useState("");
+  const [img, setImg] = useState("");
+  const [video, setVideo] = useState("");
+  const [progress, setProgress] = useState(0);
+
+  useEffect(() => {
+    img && setValue((prev) => prev + `<p><image src="${img.url}"/></p>`);
+  }, [img]);
+
+  useEffect(() => {
+    video &&
+      setValue(
+        (prev) => prev + `<p><iframe class="ql-video" src="${video.url}"/></p>`
+      );
+  }, [video]);
+
   const navigate = useNavigate();
   const { getToken } = useAuth();
 
@@ -44,13 +59,14 @@ const WritePage = () => {
     const formData = new FormData(e.target);
 
     const data = {
+      img: cover.filePath || "",
       title: formData.get("title"),
       category: formData.get("category"),
       desc: formData.get("desc"),
       content: value,
     };
 
-    //console.log(data);
+    console.log(data);
     mutation.mutate(data);
   };
 
@@ -58,11 +74,28 @@ const WritePage = () => {
     <div className="h-[calc(100vh-64px)] md:h-[calc(100vh-80px)] flex flex-col gap-6">
       <h1 className="text-cl font-light">Create a New Post</h1>
       <form onSubmit={handleSubmit} className="flex flex-col gap-6 flex-1 mb-6">
-        <button>Add a cover image</button>
-        <input name="title" type="text" placeholder="My Awesome Story" className="text-4xl font-semibold bg-transparent outline-none"/>
+        <Upload type="image" setProgress={setProgress} setData={setCover}>
+          <button className="w-max p-2 shadow-md rounded-xl text-sm text-gray-500 bg-white">
+            Add a cover image
+          </button>
+        </Upload>
+
+        <input
+          className="text-4xl font-semibold bg-transparent outline-none"
+          type="text"
+          placeholder="My Awesome Story"
+          name="title"
+        />
         <div className="flex items-center gap-4">
-          <label htmlFor="" className="text-sm">Choose a category:</label>
-          <select name="category" id="">
+          <label htmlFor="" className="text-sm">
+            Choose a category:
+          </label>
+
+          <select
+            name="category"
+            id=""
+            className="p-2 rounded-xl bg-white shadow-md"
+          >
             <option value="general">General</option>
             <option value="web-design">Web Design</option>
             <option value="development">Development</option>
@@ -71,18 +104,43 @@ const WritePage = () => {
             <option value="marketing">Marketing</option>
           </select>
         </div>
-        <textarea name="desc" placeholder="A Short Description" className="p-4 rounded-xl bg-white shadow-md"/>
-        <div className="flex">
+        <textarea
+          className="p-4 rounded-xl bg-white shadow-md"
+          name="desc"
+          placeholder="A Short Description"
+        />
+
+        <div className="flex flex-1 ">
           <div className="flex flex-col gap-2 mr-2">
-            <div className="cursor-pointer">@</div>
-            <div className="cursor-pointer">$</div>
+            <Upload type="image" setProgress={setProgress} setData={setImg}>
+              🌆
+            </Upload>
+
+            <Upload type="video" setProgress={setProgress} setData={setVideo}>
+              ▶️
+            </Upload>
           </div>
-          <ReactQuill value={value} onChange={setValue} theme="snow" className="flex-1 rounded-xl bg-white shadow-md"/>
+
+          <ReactQuill
+            theme="snow"
+            className="flex-1 rounded-xl bg-white shadow-md"
+            value={value}
+            onChange={setValue}
+            readOnly={0 < progress && progress < 100}
+          />
         </div>
-        <button disabled={mutation.isPending} className="bg-blue-800 text-white font-medium rounded-xl mt-4 p-2 w-36 disabled:bg-blue-400 disabled:cursor-not-allowed">{mutation.isPending ? "Loading..." : "Send"}</button>
+        <button
+          disabled={mutation.isPending || (0 < progress && progress < 100)}
+          className="bg-blue-800 text-white font-medium rounded-xl mt-4 p-2 w-36 disabled:bg-blue-400 disabled:cursor-not-allowed"
+        >
+          {mutation.isPending ? "Loading..." : "Send"}
+        </button>
+        {"Progress:" + progress}
+
+        {/* {mutation.isError && <span>{mutation.error.message}</span>} */}
       </form>
     </div>
-  )
-}
+  );
+};
 
 export default WritePage;
